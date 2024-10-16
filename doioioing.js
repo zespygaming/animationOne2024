0f0b01efc20a666e4655bd314b572edbcd0ac747
MedicGaming <not-hisalt712@hotmail.com>
2024-10-16 14:51:50 -0400

    a

--- doioioing.js
+++ doioioing.js
@@ -1,13 +1,13 @@
 	function doTheTriggerV2(){
-		if(document.getElementById('w2a2v2').style.display=='none'){
-			document.getElementById('w2a2v1').style.display='none';
-			document.getElementById('w2a2v2').style.display='block';
-
-			document.getElementById('triggerV2').style='background-color:black;color:white;';
-		}else{
+		if(document.getElementById('w2a2v2').style.display=='block'){
 			document.getElementById('w2a2v1').style.display='block';
 			document.getElementById('w2a2v2').style.display='none';
 
 			document.getElementById('triggerV2').style='background-color:white;color:black;';
+		}else{
+			document.getElementById('w2a2v1').style.display='none';
+			document.getElementById('w2a2v2').style.display='block';
+
+			document.getElementById('triggerV2').style='background-color:black;color:white;';
 		}
 	}
