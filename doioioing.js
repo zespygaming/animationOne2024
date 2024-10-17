f633ae7b61483cc560026f11720d3aee063dd098
MedicGaming <not-hisalt712@hotmail.com>
2024-10-17 10:31:37 -0400

    sho

--- doioioing.js
+++ doioioing.js
@@ -1,13 +1,15 @@
 	function doTheTriggerV2(){
-		if(document.getElementById('w2a2v2').style.display=='block'){
+		if(document.getElementById('triggerVV2').innerHTML=='sho'){
 			document.getElementById('w2a2v1').style.display='block';
 			document.getElementById('w2a2v2').style.display='none';
 
 			document.getElementById('triggerV2').style='background-color:white;color:black;';
+			document.getElementById('triggerVV2').innerHTML='hid';
 		}else{
 			document.getElementById('w2a2v1').style.display='none';
 			document.getElementById('w2a2v2').style.display='block';
 
 			document.getElementById('triggerV2').style='background-color:black;color:white;';
+			document.getElementById('triggerVV2').innerHTML='sho';
 		}
 	}
