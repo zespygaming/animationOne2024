2c060bcb79be0975224872543b69a4d395701667
MedicGaming <not-hisalt712@hotmail.com>
2024-10-17 14:45:18 -0400

    a

--- doioioing.js
+++ doioioing.js
@@ -4,12 +4,14 @@
 			document.getElementById('w2a2v2').style.display='none';
 
 			document.getElementById('triggerV2').style='background-color:white;color:black;';
+			document.getElementById('triggerV2').innerHTML='V';
 			document.getElementById('triggerVV2').innerHTML='hid';
 		}else{
 			document.getElementById('w2a2v1').style.display='none';
 			document.getElementById('w2a2v2').style.display='block';
 
 			document.getElementById('triggerV2').style='background-color:black;color:white;';
+			document.getElementById('triggerV2').innerHTML='Q';
 			document.getElementById('triggerVV2').innerHTML='sho';
 		}
 	}
